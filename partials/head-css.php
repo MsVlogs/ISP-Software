0ec931cdf16fbfe92dcd89c2e69b7b6549b4014b
Asraful Kabir <githubbdms@gmail.com>
2026-08-19 08:45:23 +0600

    load command center dashboard styles

--- partials/head-css.php
+++ partials/head-css.php
@@ -17,8 +17,9 @@
   <link rel="stylesheet" href="assets/css/style.css">
   <!-- ISP-Software UI v2 visual system -->
   <link rel="stylesheet" href="assets/css/isp-ui-v2.css">
+  <link rel="stylesheet" href="assets/css/isp-dashboard-v2.css">
   <link href="assets/js/lib/sweetalert2/sweetalert2.min.css" rel="stylesheet" type="text/css" />
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGSL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
   <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
   <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
   <link href="https://cdn.jsdelivr.net/npm/@mdi/font/css/materialdesignicons.min.css" rel="stylesheet">
